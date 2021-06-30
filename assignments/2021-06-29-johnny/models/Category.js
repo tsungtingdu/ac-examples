b3d1372ad0fe9dfab997fcf5283513736c707e86
tsungtingdu <tsungtingdu@gmail.com>
2021-06-30 09:32:05 +0800

    2021-06-29-johnny

---
+++ assignments/2021-06-29-johnny/models/Category.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+const categorySchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  icon: {
+    type: String,
+    required: true
+  }
+})
+module.exports = mongoose.model('Category',categorySchema)
---
+++ assignments/2021-06-29-johnny/models/Record.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+const recordSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  category: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: String,
+    required: true
+  },
+  amount: {
+    type: Number,
+    required: true
+  },
+  icon: {
+    type: String,
+    required: false
+  }
+})
+module.exports = mongoose.model('Record',recordSchema)
